f40fc219cc1aa4e1d87f360a24748417356a97d1
00Programmer00 <dahnenko02501@gmail.com>
2017-09-11 10:46:44 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './Components/Main';
+import Posts from './Components/Posts';
+import Gallery from './Components/Gallery';
+import Users from './Components/Users';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+const App = () => (
+    <Router>
+        <div>
+            <nav className="navbar navbar-inverse text-center" >
+                <div className="container text-center">
+                    <ul className="nav navbar-nav" id="navigation">
+                        <li><Link to ="/posts">Posts</Link></li>
+                        <li><Link to ="/gallery">Gallery</Link></li>
+                        <li><Link to ="/users">Users</Link></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <Route exact={true} path="/" component={Main}/>
+            <Route exact={true} path="/posts" component={Posts}/>
+            <Route exact={true} path="/gallery" component={Gallery}/>
+            <Route exact={true} path="/users" component={Users}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    </Router>
+);
 
 export default App;
